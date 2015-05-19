ede842aa72a6c96f5452c2666e095f356dd551ec
Guilhem Lettron <guilhem+github@lettron.fr>
2015-05-19 11:07:54 +0200

    Add source_url and issues_url

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'p.bergsmann@opendo.at'
 license 'apache2'
 description 'Installs/Configures locales'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url 'https://github.com/redguide/locales'
+issues_url 'https://github.com/redguide/locales/issues'
 version '0.4.0'
 
 supports 'ubuntu'
