34ec0919fb9de23aa9eb6878eb5cf91b2ff22d8b
Nathan James <nathan.james@yakara.com>
2023-02-10 15:29:08 +0000

    fix locale file path

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,10 +4,6 @@ default['locales']['packages'] = if platform_family?('debian')
                                    []
                                  end
 
-default['locales']['locale_file'] = if platform?('ubuntu')
-                                      '/var/lib/locales/supported.d/local'
-                                    else
-                                      '/etc/locale.gen'
-                                    end
+default['locales']['locale_file'] = '/etc/locale.gen'
 
 default['locales']['default'] = 'en_US'
